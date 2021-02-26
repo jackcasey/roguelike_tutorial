3622e94a29f3432ac518ce5e9fa63a90f8eb5a89
Jack Casey <beetlefeet@gmail.com>
2021-02-26 13:48:49 +0800

    Part4 added fov - tasty

--- engine.py
+++ engine.py
@@ -2,6 +2,7 @@ from typing import Set, Iterable, Any
 
 from tcod.context import Context
 from tcod.console import Console
+from tcod.map import compute_fov
 
 from entity import Entity
 from game_map import GameMap
@@ -26,6 +27,7 @@ class Engine:
             player=self.player
         )
         self.set_game_map(game_map)
+        self.update_fov()
 
     def set_game_map(self, new_game_map: GameMap):
         self.game_map = new_game_map
@@ -39,12 +41,27 @@ class Engine:
 
             action.perform(self, self.player)
 
+            self.update_fov()  # Update the FOV before the players next action.
+
+    def update_fov(self) -> None:
+        """Recompute the visible area based on the players point of view."""
+        self.game_map.visible[:] = compute_fov(
+            self.game_map.tiles["transparent"],
+            (self.player.x, self.player.y),
+            radius=8,
+        )
+        # If a tile is "visible" it should be added to "explored".
+        self.game_map.explored |= self.game_map.visible
+
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
 
         for entity in self.entities:
-            console.print(entity.x, entity.y, entity.char, fg=entity.color)
+            # Only print entities that are in the FOV
 
-            context.present(console)
+            if self.game_map.visible[entity.x, entity.y]:
+                console.print(entity.x, entity.y, entity.char, fg=entity.color)
 
-            console.clear
+        context.present(console)
+
+        console.clear
